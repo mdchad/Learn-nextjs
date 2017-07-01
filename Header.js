2887eabc4dfc91043c04674f806c948f86db4cef
Irsyad <irsyad.muhd@gmail.com>
2017-07-02 02:26:17 +0800

    done with lesson 'using shared component'

---
+++ Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Link from 'next/link'
+
+const linkStyle = {
+    marginRight: 15,
+    color: 'red'
+}
+
+const Header = () => (
+    <div>
+        <Link href="/">
+            <a style={linkStyle}>Home</a>
+        </Link>
+        <Link href="/about">
+            <a style={linkStyle}>About</a>
+        </Link>
+    </div>
+)
+
+export default Header
